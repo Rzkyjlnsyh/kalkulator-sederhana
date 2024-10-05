654fd64e5af225a812d9bd441313fac5d96de365
Rzkyjlnsyh <suryasadewa2904@gmail.com>
2024-10-06 03:40:54 +0700

    First commit

---
+++ kalkulator.py
@@ -0,0 +1,23 @@
+#operasi aritmatika
+a = int(input("Masukan angka : "))
+b = int(input("Masukan angka : "))
+
+hasil = a + b
+print(a,'+',b,'=',hasil)
+
+hasil = a - b8
+print(a,'-',b,'=',hasil)
+
+hasil = a * b
+print(a,'*',b,'=',hasil)
+
+hasil = a / b
+print(a,'/',b,'=',hasil)
+
+#eksponen (pangkat) **
+hasil = a ** b
+print(a,'**',b,'=',hasil)
+
+#modulus (sisa pembagian) %
+hasil = a % b
+print(a,'%',b,'=',hasil)
